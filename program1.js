962173408f542e663a0a1bdedad9d2763599a168
Raunak Yadush <raunakyadush@gmail.com>
2024-11-11 15:57:04 +0530

    Complete

--- program1.js
+++ program1.js
@@ -1,8 +1,32 @@
+
 const getTotalIsles = function (grid) {
 
 
   // write your code here
+  const dfs = (grid, i, j) => {
+      if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === 'W') {
+          return;
+      }
+      grid[i][j] = 'W';
+      dfs(grid, i + 1, j);
+      dfs(grid, i - 1, j);
+      dfs(grid, i, j + 1);
+      dfs(grid, i, j - 1);
+  };
 
+  let islandCount = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          if (grid[i][j] === 'L') {
+              islandCount++;
+              dfs(grid, i, j);
+          }
+      }
+  }
+
+  return islandCount;
 };
+
 
 module.exports = getTotalIsles;
